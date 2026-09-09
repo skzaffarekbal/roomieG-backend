1ac8bfbc2663b79b0515df649496580c7a961972
Sk Zaffar Ekbal <s.zaffarekbal@gmail.com>
2026-09-09 22:32:29 +0530

    request and connection user populate logic

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -6,7 +6,7 @@ const Chat = require('../model/chat');
 const { profileCompleted } = require('../middlewares/profileCompleted');
 
 const userRouter = express.Router();
-const USER_POPULATE = 'firstName lastName photo gender dateOfBirth bio createdAt';
+const USER_POPULATE = 'firstName lastName photo gender dateOfBirth bio createdAt subscription';
 
 userRouter.get('/user/request/received', userAuth, async (req, res) => {
   try {
